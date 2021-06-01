d730842866b28c9a37bdb69db17131066b27dcbf
Martin Obermeier <martin.obermeier@gmail.com>
2021-05-31 22:07:57 -0300

    Seccion 14: HeroesApp - Single Page Application (SPA)

---
+++ src/components/heroes/HeroList.js
@@ -0,0 +1,21 @@
+import React, { useMemo } from 'react'
+import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher'
+import { HeroCard } from './HeroCard';
+
+export const HeroList = ( { publisher } ) => {
+
+    //const heroes = getHeroesByPublisher( publisher );
+    const heroes = useMemo( () => getHeroesByPublisher( publisher ), [publisher] );
+
+    return (
+        <div className="card-columns animate__animated animate__fadeIn">
+            {
+                heroes.map( hero => {
+                   return <HeroCard key={ hero.id } hero={ hero } >
+                        { hero.superhero }
+                    </HeroCard>
+                })
+            }
+        </div>
+    )
+}
---
+++ src/components/heroes/HeroScreen.js
@@ -0,0 +1,63 @@
+import React, { useMemo } from 'react';
+import { Redirect, useParams } from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeroById';
+
+export const HeroScreen = ( { history } ) => {
+
+    // Hook para obtener los parametros de la url
+    const { heroId } = useParams();
+
+    //const hero = getHeroById( heroId )
+    const hero = useMemo(() => getHeroById( heroId ), [ heroId ]);
+
+    if( !hero ) {
+        return <Redirect to="/" />;
+    }
+
+    const handleReturn = () => {
+        if( history.length <= 2 ) {
+            history.push('/');
+        } else {
+            history.goBack();
+        }
+
+    }
+
+    const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters,
+    } = hero;
+
+    return (
+        <div className="row mt-5">
+            <div className="col-4">
+                <img 
+                    alt={ superhero } 
+                    className="img-thumbnail animate__animated animate__fadeInLeft" 
+                    src={ `../assets/heroes/${heroId}.jpg` } 
+                />
+            </div>
+            <div className="col-8">
+                <h3> { superhero } </h3>
+                <ul className="list-group list-group-flush" >
+                    <li className="list-group-item"> <b> Alter ego: </b> { alter_ego } </li>
+                    <li className="list-group-item"> <b> Publisher </b> { publisher } </li>
+                    <li className="list-group-item"> <b> First appearance: </b> { first_appearance } </li>
+                </ul>
+
+                <h5> Characters </h5>
+                <p> { characters } </p>
+
+                <button 
+                    className="btn btn-outline-info" 
+                    onClick={ handleReturn }
+                >
+                    Return
+                </button>
+            </div>
+        </div>
+    )
+}
